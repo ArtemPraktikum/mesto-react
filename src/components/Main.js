0d21dff1815616faaf2174be256f5d135c875aaf
Artem Velmozhin <artemvrabota@yandex.ru>
2021-09-03 15:18:21 +0400

    feat: сделать пункт 5: Добавьте императивные обработчики

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,37 @@
 function Main() {
+  const handleEditAvatarClick = () => {
+    document.querySelector('.avatar-popup').classList.add('popup_opened')
+  }
+  const handleEditProfileClick = () => {
+    document.querySelector('.profile-popup').classList.add('popup_opened')
+  }
+  const handleAddPlaceClick = () => {
+    document.querySelector('.add-popup').classList.add('popup_opened')
+  }
+
   return (
     <main className='content'>
       <section className='profile'>
         <div className='profile__avatar-section'>
           <img src='#' alt='Аватар профиля' className='profile__avatar' />
-          <button type='button' className='profile__avatar-button' />
+          <button
+            type='button'
+            className='profile__avatar-button'
+            onClick={handleEditAvatarClick}
+          />
         </div>
         <div className='profile__info'>
           <div className='profile__heading'>
             <h1 className='profile__title' />
-            <button type='button' className='profile__edit-button' />
+            <button
+              type='button'
+              className='profile__edit-button'
+              onClick={handleEditProfileClick}
+            />
           </div>
           <p className='profile__subtitle' />
         </div>
-        <button type='button' className='profile__add-button' />
+        <button type='button' className='profile__add-button' onClick={handleAddPlaceClick} />
       </section>
       <section className='elements' />
     </main>
