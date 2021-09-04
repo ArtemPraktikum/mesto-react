a2a1ae148501b9c7b4f2f3adc2a43204ed13ff6b
Artem Velmozhin <artemvrabota@yandex.ru>
2021-09-04 21:59:18 +0400

    feat: сделать пункт 6: Портируйте модуль API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,17 @@
+import { api } from '../utils/Api.js'
+
 function Main(props) {
-
-
   return (
     <main className='content'>
       <section className='profile'>
         <div className='profile__avatar-section'>
           <img src='#' alt='Аватар профиля' className='profile__avatar' />
-          <button
-            type='button'
-            className='profile__avatar-button'
-            onClick={props.onEditAvatar}
-          />
+          <button type='button' className='profile__avatar-button' onClick={props.onEditAvatar} />
         </div>
         <div className='profile__info'>
           <div className='profile__heading'>
             <h1 className='profile__title' />
-            <button
-              type='button'
-              className='profile__edit-button'
-              onClick={props.onEditProfile}
-            />
+            <button type='button' className='profile__edit-button' onClick={props.onEditProfile} />
           </div>
           <p className='profile__subtitle' />
         </div>
