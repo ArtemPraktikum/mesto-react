7db0bc75880381d85a991f81287b8e18007277fb
Artem Velmozhin <artemvrabota@yandex.ru>
2021-09-09 23:44:58 +0400

    feat: выполнить пункт 1 проект.раб. 11 до: Используйте контекст в Card

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import Header from './Header.js'
 import Main from './Main.js'
 import Footer from './Footer.js'
 import PopupWithForm from './PopupWithForm.js'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ImagePopup from './ImagePopup.js'
+import { api } from '../utils/Api.js'
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
 function App() {
   // переменные состояния, отвечающие за видимость
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
@@ -11,6 +13,21 @@ function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false)
   const [selectedCard, setSelectedCard] = useState(false)
 
+  //Юзер инфо
+  const [currentUser, setCurrentUser] = useState('')
+
+  useEffect(() => {
+    // Данные необходимые при загрузке страницы
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      .then(([userArray, cardsArray]) => {
+        setCurrentUser(userArray)
+
+        // setСards(cardsArray)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
   // функции отвечающие за изменение стейта
   const handleEditProfileClick = () => {
     setIsEditProfilePopupOpen(true)
@@ -36,118 +53,120 @@ function App() {
   }
 
   return (
-    <div className='page'>
-      <Header />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className='page'>
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
 
-      <PopupWithForm
-        popupClass='profile-popup'
-        formName='formProfile'
-        title='Редактировать профиль'
-        submitButtonText='Сохранить'
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-      >
-        <fieldset className='popup__input'>
-          <label className='popup__field'>
-            <input
-              type='text'
-              className='popup__item'
-              id='name'
-              name='nameInFormProfile'
-              defaultValue={''}
-              placeholder='Имя'
-              required
-              minLength={2}
-              maxLength={40}
-            />
-            <span className='popup__item-error name-error' />
-          </label>
-          <label className='popup__field'>
-            <input
-              type='text'
-              className='popup__item'
-              id='aboutMe'
-              name='aboutMeInFormProfile'
-              defaultValue={''}
-              placeholder='Обо мне'
-              required
-              minLength={2}
-              maxLength={200}
-            />
-            <span className='popup__item-error aboutMe-error' />
-          </label>
-        </fieldset>
-      </PopupWithForm>
-      <PopupWithForm
-        popupClass='add-popup'
-        formName='formAddCard'
-        title='Новое место'
-        submitButtonText='Создать'
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-      >
-        {/* children */}
-        <fieldset className='popup__input'>
-          <label className='popup__field'>
-            <input
-              type='text'
-              className='popup__item'
-              id='placeName'
-              name='nameInFormAddCard'
-              defaultValue={''}
-              placeholder='Название'
-              required
-              minLength={2}
-              maxLength={30}
-            />
-            <span className='popup__item-error placeName-error' />
-          </label>
-          <label className='popup__field'>
-            <input
-              type='url'
-              className='popup__item'
-              id='link'
-              name='aboutMeInFormAddCard'
-              defaultValue={''}
-              placeholder='Ссылка на картинку'
-              required
-            />
-            <span className='popup__item-error link-error' />
-          </label>
-        </fieldset>
-      </PopupWithForm>
-      <PopupWithForm
-        popupClass='avatar-popup'
-        formName='formAvatar'
-        title='Обновить аватар'
-        submitButtonText='Сохранить'
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}
-      >
-        <fieldset className='popup__input'>
-          <label className='popup__field'>
-            <input
-              type='url'
-              className='popup__item'
-              id='linkAvatar'
-              name='avatarInformAvatar'
-              defaultValue={''}
-              placeholder='Ссылка на аватар'
-              required
-            />
-            <span className='popup__item-error linkAvatar-error' />
-          </label>
-        </fieldset>
-      </PopupWithForm>
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-    </div>
+        <PopupWithForm
+          popupClass='profile-popup'
+          formName='formProfile'
+          title='Редактировать профиль'
+          submitButtonText='Сохранить'
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <fieldset className='popup__input'>
+            <label className='popup__field'>
+              <input
+                type='text'
+                className='popup__item'
+                id='name'
+                name='nameInFormProfile'
+                defaultValue={''}
+                placeholder='Имя'
+                required
+                minLength={2}
+                maxLength={40}
+              />
+              <span className='popup__item-error name-error' />
+            </label>
+            <label className='popup__field'>
+              <input
+                type='text'
+                className='popup__item'
+                id='aboutMe'
+                name='aboutMeInFormProfile'
+                defaultValue={''}
+                placeholder='Обо мне'
+                required
+                minLength={2}
+                maxLength={200}
+              />
+              <span className='popup__item-error aboutMe-error' />
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm
+          popupClass='add-popup'
+          formName='formAddCard'
+          title='Новое место'
+          submitButtonText='Создать'
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+        >
+          {/* children */}
+          <fieldset className='popup__input'>
+            <label className='popup__field'>
+              <input
+                type='text'
+                className='popup__item'
+                id='placeName'
+                name='nameInFormAddCard'
+                defaultValue={''}
+                placeholder='Название'
+                required
+                minLength={2}
+                maxLength={30}
+              />
+              <span className='popup__item-error placeName-error' />
+            </label>
+            <label className='popup__field'>
+              <input
+                type='url'
+                className='popup__item'
+                id='link'
+                name='aboutMeInFormAddCard'
+                defaultValue={''}
+                placeholder='Ссылка на картинку'
+                required
+              />
+              <span className='popup__item-error link-error' />
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm
+          popupClass='avatar-popup'
+          formName='formAvatar'
+          title='Обновить аватар'
+          submitButtonText='Сохранить'
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+        >
+          <fieldset className='popup__input'>
+            <label className='popup__field'>
+              <input
+                type='url'
+                className='popup__item'
+                id='linkAvatar'
+                name='avatarInformAvatar'
+                defaultValue={''}
+                placeholder='Ссылка на аватар'
+                required
+              />
+              <span className='popup__item-error linkAvatar-error' />
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </div>
+    </CurrentUserContext.Provider>
   )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
 import { api } from '../utils/Api.js'
 import Card from './Card.js'
 function Main(props) {
-  // переменные состояния, отвечающие за данные пользователя
-  const [userName, setUserName] = useState('')
-  const [userDescription, setUserDescription] = useState('')
-  const [userAvatar, setUserAvatar] = useState('')
+  const traverseUserContext = useContext(CurrentUserContext)
   // переменные состояния, отвечающие за карточки
   const [cards, setСards] = useState([])
 
@@ -13,9 +11,6 @@ function Main(props) {
     // Данные необходимые при загрузке страницы
     Promise.all([api.getUserInfo(), api.getInitialCards()])
       .then(([userArray, cardsArray]) => {
-        setUserName(userArray.name)
-        setUserDescription(userArray.about)
-        setUserAvatar(userArray.avatar)
 
         setСards(cardsArray)
       })
@@ -23,20 +18,19 @@ function Main(props) {
         console.log(error)
       })
   }, [])
-
   return (
     <main className='content'>
       <section className='profile'>
         <div className='profile__avatar-section'>
-          <img src={userAvatar} alt='Аватар профиля' className='profile__avatar' />
+          <img src={traverseUserContext.avatar} alt='Аватар профиля' className='profile__avatar' />
           <button type='button' className='profile__avatar-button' onClick={props.onEditAvatar} />
         </div>
         <div className='profile__info'>
           <div className='profile__heading'>
-            <h1 className='profile__title'>{userName}</h1>
+            <h1 className='profile__title'>{traverseUserContext.name}</h1>
             <button type='button' className='profile__edit-button' onClick={props.onEditProfile} />
           </div>
-          <p className='profile__subtitle'>{userDescription}</p>
+          <p className='profile__subtitle'>{traverseUserContext.about}</p>
         </div>
         <button type='button' className='profile__add-button' onClick={props.onAddPlace} />
       </section>
